f11499d35be3b6cec80c3378496f97c897234eff
Moisés Ñañez <mnanez@able.co>
2018-10-31 16:51:23 -0500

    Use spaces instead of tabs as indentation

--- packages/commitlint-config-emoji-log/index.js
+++ packages/commitlint-config-emoji-log/index.js
@@ -11,21 +11,33 @@
  */
 
 module.exports = {
-	parserPreset: {
-		parserOpts: {
-			headerPattern: /^(.*\w*): (.*)$/,
-			headerCorrespondence: ['type', 'subject']
-		}
-	},
-	rules: {
-		'body-leading-blank': [1, 'always'],
-		'footer-leading-blank': [1, 'always'],
-		'header-max-length': [2, 'always', 72],
-		'scope-case': [2, 'always', 'lower-case'], // ?
-		'subject-empty': [2, 'never'],
-		'subject-full-stop': [2, 'never', '.'],
-		'type-empty': [2, 'never'],
-		'type-case': [2, 'always', 'upper-case'],
-		'type-enum': [2, 'always', ['📦 NEW', '👌 IMPROVE', '🐛 FIX', '🚀 RELEASE', '🦄 RELEASE', '📖 DOC', '✅ TEST']]
-	}
+  parserPreset: {
+    parserOpts: {
+      headerPattern: /^(.*\w*): (.*)$/,
+      headerCorrespondence: ["type", "subject"]
+    }
+  },
+  rules: {
+    "body-leading-blank": [1, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 72],
+    "scope-case": [2, "always", "lower-case"], // ?
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-empty": [2, "never"],
+    "type-case": [2, "always", "upper-case"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "📦 NEW",
+        "👌 IMPROVE",
+        "🐛 FIX",
+        "🚀 RELEASE",
+        "🦄 RELEASE",
+        "📖 DOC",
+        "✅ TEST"
+      ]
+    ]
+  }
 };
